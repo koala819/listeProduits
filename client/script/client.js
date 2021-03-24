5b3fc7cca861cd6be758a576c948163fe57008fe
koala819 <x.genolhac@gmail.com>
2021-03-24 13:28:49 +0100

    new use of function map
    begin use loader

--- client/script/client.js
+++ client/script/client.js
@@ -1,18 +1,30 @@
+/* const box = document.querySelector('.box');
+const txt = document.querySelector('.txt');
+const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+}) */
+
 function populateTableList() {
   fetch(`http://127.0.0.1:8000/box`)
     .then(res => res.json())
     .then(res => {
-      let html = '';
-      res.map((p) => {
-        console.log(p);
-        html += '<tr class="text-center">' +
+      return res.map((p) => {
+        return '<tr class="text-center">' +
           '<td class="inline-block align-middle"><img src=' + p.picture + ' ></td>' +
           '<td class="w-1/4">' + p.name + '</td>' +
           '<td class="w-1/4">' + p.price + '€</td>' +
           '<td class="w-1/4"><button class="btn btn-info">View</button></td>' +
           '</tr>';
       })
-      return (html);
     })
     .then(pouet => {
       document.getElementById('productList').innerHTML = pouet;
